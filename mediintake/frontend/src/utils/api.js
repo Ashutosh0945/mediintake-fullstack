1a1d59b3370dd2b623f0fa53c01c01caa4e18d61
Ashutosh Singh <ashutosh.236437109@vcet.edu.in>
2026-03-03 00:06:52 +0530

    fix api url

--- mediintake/frontend/src/utils/api.js
+++ mediintake/frontend/src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://mediintake-fullstack.onrender.com/api",
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
